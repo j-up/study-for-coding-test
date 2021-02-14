8cc15093084dd568d18e89a7b6a9a792d454fde2
JM <ad4674@gmail.com>
2021-02-14 15:29:25 +0900

    이진트리탐색을 이용한 문제구현

--- part7/5-for-java.java
+++ part7/5-for-java.java
@@ -2,46 +2,34 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int result = 0;
+        Integer[] items = {8, 3, 7, 9, 2};
+        Integer[] targetItems = {5, 7, 9};
+        String[] resultArray = new String[targetItems.length];
 
-        Integer[] a = {6,7,3,2,1,8,9,4};
-        // 이진 탐색은 반드시 정렬되어있어야함
-        Arrays.sort(a);
+        Arrays.sort(items);
 
-        result = binarySearch(a, 4, 0, a.length);
-        System.out.println(Arrays.toString(a));
-        System.out.println(result);
+        for(int i = 0; i<targetItems.length; i++) {
+            resultArray[i] = binarySearch(items, targetItems[i], 0, items.length);
+        }
+        System.out.println(Arrays.toString(resultArray));
 
-    }
-    /*
-    이진 탐색 (재귀)
-     */
-    public static int binarySearch(Integer[] array, int target, int start, int end) {
-        if(start>end) return -1;
-
-        int mid = (start+end) / 2;
-
-        if(array[mid]==target) return mid;
-
-        else if(array[mid] > target) return binarySearch(array, target, start, mid-1);
-
-        else return binarySearch(array, target, mid+1, end);
+        for(int j = 0; j<resultArray.length; j++) {
+            System.out.print(resultArray[j]);
+        }
     }
 
-    /*
-    이진 탐색 (반복문)
-     */
-    public static int binarySearch(Integer[] array, int target, int start, int end) {
-
-        while(start<=end) {
+    public static String binarySearch(Integer[] array, int target, int start, int end) {
+        while(start<end) {
             int mid = (start+end) / 2;
-            if(array[mid] == target) return mid;
 
-            else if(array[mid] > target) end = mid -1;
+            if(array[mid] == target) return "yes";
 
-            else if(array[mid] < target) start = mid +1;
+            else if(array[mid] > target) end = mid - 1;
+
+            else start = mid + 1;
         }
 
-        return -1;
+        return "no";
     }
+
 }
