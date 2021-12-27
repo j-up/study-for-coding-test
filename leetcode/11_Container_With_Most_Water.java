f5d8f42107e4957600588c842404de0d0aa9606d
JM <ad4674@gmail.com>
2021-12-27 21:39:50 +0900

    시간 이슈 해결

--- leetcode/11_Container_With_Most_Water.java
+++ leetcode/11_Container_With_Most_Water.java
@@ -1,15 +1,22 @@
 class Solution {
     public int maxArea(int[] height) {
-        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+        int max_area = Math.min(height[left], height[right]) * (right - left);
 
-
-        for(int i=0;i<height.length-1;i++){
-            for(int j=i+1;j<height.length;j++){
-                int area = Math.min(height[i],height[j]) * (j-i);
-                maxArea = Math.max(maxArea,area);
+        while (left < right) {
+            System.out.println("height[left]: " + height[left] + ", height[right]: " + height[right]);
+            System.out.println("left: " + left + " right: " + right);
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
             }
+
+            int area = Math.min(height[left], height[right]) * (right - left);
+            max_area = Math.max(max_area, area);
         }
 
-        return maxArea;
+        return max_area;
     }
 }
